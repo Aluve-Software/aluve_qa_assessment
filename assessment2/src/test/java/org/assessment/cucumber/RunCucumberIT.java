d258f77617d2318cd18c9583efb6edda32a71f3d
Sibonelo <sibonelo@aluvesqe.co.za>
2023-09-18 11:35:20 +0200

    Second assessment

---
+++ assessment2/src/test/java/org/assessment/cucumber/RunCucumberIT.java
@@ -0,0 +1,17 @@
+package org.assessment.cucumber;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue="tech/aluvesoftware/steps",
+        tags="@search"
+)
+
+public class RunCucumberIT {
+
+}
